00335dd52749c70db9a73056d2ccca450c2659ec
Jmayora1512 <jorgemayora88@gmail.com>
2023-02-02 19:01:26 +0100

    Se agrega el proyecto

---
+++ src/web/Servlet.java
@@ -0,0 +1,76 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/ServletHeaders")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String metodoHttp = request.getMethod();
+        out.print("<html>");
+        out.print("<head>");
+        out.print("<title>Headers HTTP</title>");
+        out.print("</head>");
+        out.print("<body>");
+        out.print("<h1>Headers HTTP</h1>");
+        out.print("<br>");
+        out.print("Metodo Http:" + metodoHttp);
+        
+        String uri = request.getRequestURI();
+        out.print("<br>");
+        out.print("Uri solicitada:" + uri);
+        
+        //Imprimimos todos los cabeceros disponibles
+        out.print("<br>");
+        out.print("<br>");
+        Enumeration cabeceros = request.getHeaderNames();
+        while(cabeceros.hasMoreElements()){
+            String nombreCabecero = (String) cabeceros.nextElement();
+            out.print("<b>" + nombreCabecero + "</b>:");
+            out.print(request.getHeader(nombreCabecero));
+            out.print("<br>");
+        }
+        
+        out.print("<body>");
+        out.print("</html>");
+        out.close();		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
